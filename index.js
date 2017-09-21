ba317bb00b868dfabdffcd96934fd53588b67f50
Kelvin Luck <kelvin.luck@crowdstrike.com>
2017-09-21 13:39:38 +0100

    Use prefered method of locating actual app instance
    Newer versions of ember-cli have a `_findHost` function to do this for
    us but for backward compatibility we also polyfill the functionality
    when it's not available.

--- index.js
+++ index.js
@@ -4,12 +4,24 @@
 module.exports = {
   name: 'ember-chrome-devtools',
 
-  included(parent) {
-    this.isProductionBuild = parent.env === 'production';
+  findHost() {
+    // `ember-cli` >= 2.7.0 has _findHost
+    if (typeof this._findHost === 'function') {
+      return this._findHost();
+    }
+
+    // Otherwise we polyfill
+    let app;
+    let current = this;
+    do {
+      app = current.app || app;
+    } while (current.parent.parent && (current = current.parent));
+    return app;
   },
 
   treeFor(name) {
-    if (this.isProductionBuild) {
+    let app = this.findHost();
+    if (app.env === 'production') {
       return;
     }
 
